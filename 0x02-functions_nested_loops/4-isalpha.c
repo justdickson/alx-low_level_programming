cc5d9e26a6e9ad6ab513cf16814750ab0f94b1d6
justdick <dickson.oreal@gmail.com>
2023-06-21 12:58:57 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
  * _isalpha checks whether character is lowercase or uppercase
+ * @c:character to check if it islowercase
  * Return:1- if lowercase/uppercase,0-if not
  */
 
