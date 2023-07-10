2f66e7d49f74bd261edd9d1fe0c08ea77a24915d
justdick <dickson.oreal@gmail.com>
2023-07-10 11:40:22 -0700

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -20,11 +20,8 @@
 int main(int argc, char *argv[])
                 
 {
-                
-        int i;
-                
-                
-        for (i = 0; i < argc; ++i)
+ 
+        for (int i = 0; i < argc; ++i)
                 
         {
                 
